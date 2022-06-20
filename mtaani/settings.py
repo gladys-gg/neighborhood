041a1fdc2f547d0742da97fe013596f0d2cdc752
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-06-20 21:50:49 +0300

    implementing the database path and changing it to postgresql

--- mtaani/settings.py
+++ mtaani/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'mtaani.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mtaani',
+        'USER': 'acer',
+        'PASSWORD':'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
