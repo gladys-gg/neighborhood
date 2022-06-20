8f551cdc5b7abe615089535b8f16195d6292bb5a
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-06-20 22:11:15 +0300

    Added the authentication and apps urls to the projects urls path

--- mtaani/urls.py
+++ mtaani/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.contrib.auth import views
+from hood import views
+from django.contrib.auth import views as auth_views
+from hood.views import profile
 
 urlpatterns = [
+    path('', include('hood.urls')),
     path('admin/', admin.site.urls),
+            # User Authentication
+
+    path('sign-up/',views.register,name='sign-up'),
+    path('login/',auth_views.LoginView.as_view(template_name = 'registration/sign-in.html'),name='sign-in'),
+
+    path('user/', profile, name='profile'),
 ]
