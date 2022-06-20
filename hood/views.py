f07e8bdc1f23ce7520af74d6218a9a06edc08da2
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-06-20 21:58:49 +0300

    implemented the homepage view

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render,redirect
+from .models import Business, NeighbourHood, Post, Profile
+from django.contrib.auth.decorators import login_required
+from .forms import UpdateProfileForm,NeighbourhoodForm,PostForm,BusinessForm, UserRegisterForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
+def index(request):
+    mtaani = NeighbourHood.objects.all()
+    return render(request, 'index.html',{'mtaani':mtaani})
