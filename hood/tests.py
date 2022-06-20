d40c56457c4198e5e842d01a1bfeaae612c42a90
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-06-21 02:42:08 +0300

    testing the tests

--- hood/tests.py
+++ hood/tests.py
@@ -10,8 +10,8 @@ class NeighbourhoodTestClass(TestCase):
     def setUp(self):
         # create an admin user
         self.admin = User.objects.create_superuser(
-            username='sweet',
-            password='bitter123'
+            username='gladys',
+            password='gladys'
         )
         self.neighbourhood = NeighbourHood(
             name='Test Neighbourhood', location=self.location, occupants_count=10, admin_id=self.admin.id)
@@ -36,7 +36,7 @@ class BusinessTestCase(TestCase):
     setup
     '''
     def setUp(self):
-        self.business = Business(name='soko',image='soko.jpeg',pub_date='12,Oct,2018',user='1',NeighborHood='1')
+        self.business = Business(name='boutique',image='cloudinary image',user='1',NeighborHood='1')
     '''
     test instance of business
     '''
